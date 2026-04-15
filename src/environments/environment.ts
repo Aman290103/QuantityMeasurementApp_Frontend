2e1f1856b0ab98854fd70ce5d73793e68f0b4e79
Aman290103 <amankumarchaubey2901@gmail.com>
2026-04-15 22:01:55 +0530

    Update API URL to new Render backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://quantitymeasurementapp-api-rdwr.onrender.com/api/v1'
+  apiUrl: 'https://quantitymeasurementapp-4ys1.onrender.com/api/v1'
 };
